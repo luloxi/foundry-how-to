bbcfa6dfc3a4dad404f22176efd362d8c33e17cd
Lulox <ligma@duck.com>
2023-03-28 13:03:02 -0300

    About to install a library

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
@@ -9,16 +9,29 @@ contract CounterTest is Test {
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    // Tests must start with "test" keyword
+    function testInc() public {
+        counter.inc();
+        assertEq(counter.count(), 1);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    // "Fail" keyword indicates a test is expected to fail
+    function testFailDec () public {
+        counter.dec();
+    }
+
+    function testDecUnderflow() public {
+        // Here we are more specific than in testFailDec about the type of error
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
+    }
+
+    function testDec() public {
+        counter.inc();
+        counter.inc();
+        counter.dec();
+        assertEq(counter.count(), 1);
     }
 }
